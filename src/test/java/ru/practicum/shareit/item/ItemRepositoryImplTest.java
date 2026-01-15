9f2b94841b4a6261071780606fc0564123fc838f
Nikita Ukhov <exclave.exe@yandex.ru>
2026-01-15 11:30:54 +0500

    fix: import

--- src/test/java/ru/practicum/shareit/item/ItemRepositoryImplTest.java
+++ src/test/java/ru/practicum/shareit/item/ItemRepositoryImplTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
-import java.lang.reflect.Field;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
--- src/test/java/ru/practicum/shareit/user/UserRepositoryImplTest.java
+++ src/test/java/ru/practicum/shareit/user/UserRepositoryImplTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.practicum.shareit.user.model.User;
 
-import java.lang.reflect.Field;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserRepositoryImplTest {
