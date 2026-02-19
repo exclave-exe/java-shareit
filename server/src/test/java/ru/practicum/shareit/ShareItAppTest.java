4ea8ee40413ed692559b9ba58fd8c0d711a44bdc
Nikita Ukhov <exclave.exe@yandex.ru>
2026-02-19 19:46:37 +0500

    fix: testname

--- server/src/test/java/ru/practicum/shareit/ShareItAppTest.java
+++ server/src/test/java/ru/practicum/shareit/ShareItAppTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ShareItServerApplicationTest {
+class ShareItAppTest {
 
     @Test
     void contextLoads() {
