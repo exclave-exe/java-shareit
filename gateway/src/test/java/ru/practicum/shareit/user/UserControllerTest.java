8411b6b446338d5ddf53beba744cbccb09e2d6d8
Nikita Ukhov <exclave.exe@yandex.ru>
2026-02-20 21:31:42 +0500

    fix: codestyle

--- gateway/src/test/java/ru/practicum/shareit/user/UserControllerTest.java
+++ gateway/src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
